049582bed66a7cb8eef60a124a1d6c5360c93d99
Matvii Strechen <mvstrechen@gmail.com>
2019-01-06 21:32:24 +0200

    Seacrh and feed methods now also return text

--- src/search/methods.py
+++ src/search/methods.py
@@ -8,6 +8,7 @@ def feed(args : dict):
     tmp[0]["datetime"] = str(datetime(2018, 1, 5, 11, 59))
     tmp[0]["img"] = "https://img.tsn.ua/cached/1533895990/tsn-ec97a3c0a2ace5bfabc1ed73666af320/thumbs/315x210/84/c4/4b48271dd867e71b5db3bbb6ecabc484.jpg" 
     tmp[0]["source"] = "TSN"
+    tmp[0]["text"] = "Something"
     return json.dumps(tmp, ensure_ascii=False)
     
 
@@ -17,10 +18,12 @@ def search(args : dict):
     tmp[0]["datetime"] = str(datetime(2018, 1, 5, 11, 59))
     tmp[0]["img"] = "https://img.tsn.ua/cached/1533895990/tsn-ec97a3c0a2ace5bfabc1ed73666af320/thumbs/315x210/84/c4/4b48271dd867e71b5db3bbb6ecabc484.jpg" 
     tmp[0]["source"] = "TSN"
+    tmp[0]["text"] = "Something"
     tmp[1]["headline"] = u"Спецслужби Росії готують інформаційні провокації проти України (ВІДЕО) "
     tmp[1]["datetime"] = str(datetime(2018, 1, 5, 16, 21))
     tmp[1]["img"] = ""
-    tmp[1]["source"] = "UA|TV" 
+    tmp[1]["source"] = "UA|TV"
+    tmp[1]["text"] = "Something other" 
     
     return json.dumps(tmp, ensure_ascii=False)
     
