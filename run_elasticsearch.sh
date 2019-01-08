b09e6e991876302842c45b95cc272a147933ae12
Matvii Strechen <mvstrechen@gmail.com>
2019-01-08 21:09:19 +0200

    Fix elasticsearch script
    Docker now mounts a volume for saving data between sessions

--- run_elasticsearch.sh
+++ run_elasticsearch.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --name elastic --rm -it -i -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
+docker run --name elastic \
+--mount source=elasticdata,target="/usr/share/elasticsearch/data" \
+--rm -it -i -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
