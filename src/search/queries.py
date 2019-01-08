debf84c910f6bb9ac87420c70d13a1cfc7dfd416
Matvii Strechen <mvstrechen@gmail.com>
2019-01-08 23:56:53 +0200

    Add search function

--- src/search/queries.py
+++ src/search/queries.py
@@ -6,8 +6,8 @@ def es_result_to_list(res: dict):
     res_list = []
     for r in res["hits"]["hits"]:
         res_list.append(r["_source"])
-    
     return res_list
+
 
 def get_feed(offset: int = 0, limit: int = 10):
     body = {"query": {"match_all": {}},
@@ -17,5 +17,21 @@ def get_feed(offset: int = 0, limit: int = 10):
             "from" : offset,
             "size" : limit
             }
+    res = es.search(index="news", body=body)
+    return es_result_to_list(res)
+
+def get_searh_results(query:str, offset: int = 0, limit: int = 10):
+    body = {
+        "query": {
+            "multi_match" : {
+                "query":      query,
+                "type":       "best_fields",
+                "fields":     [ "headline", "text" ],
+                "tie_breaker": 0.3
+                }
+            },
+        "from" : offset,
+        "size" : limit
+    }
     res = es.search(index="news", body=body)
     return es_result_to_list(res)
