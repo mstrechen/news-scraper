7b6944919cf69aa2890bc1f54930772c67cec6aa
Matvii Strechen <mvstrechen@gmail.com>
2019-01-10 00:19:16 +0200

    Article constructor now make url and headline without leading spaces

--- src/scraping/article.py
+++ src/scraping/article.py
@@ -4,15 +4,15 @@ from urllib import parse
 from elasticsearch import Elasticsearch
 
 class Article:
-    def __init__(self, url: str, headline: str, dt: datetime = datetime.now()):
-        self.url = url
+    def __init__(self, url: str, headline: str, dt: datetime = datetime.now(), tags: list = None):
+        self.url = url.strip()
         self.datetime = dt
-        self.headline = headline
+        self.headline = headline.strip()
         self.img = ""
         self.text = ""
         self.richtext = ""
         self.src_name = ""
-        self.tags = []
+        self.tags = [] if tags is None else tags
 
 
     def get_source(self):
