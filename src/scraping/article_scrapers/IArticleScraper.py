2c7d5f119afbd7dc89374b6e4fd2c2d021249bc3
Matvii Strechen <mvstrechen@gmail.com>
2019-01-10 09:15:47 +0200

    Minor refactoring

--- src/scraping/article_scrapers/IArticleScraper.py
+++ src/scraping/article_scrapers/IArticleScraper.py
@@ -1,10 +1,8 @@
 from abc import ABC, abstractmethod
+from urllib import parse
 
 from lxml import etree
 from bs4 import BeautifulSoup
-from urllib import parse
-
-from elasticsearch import Elasticsearch
 
 from ..imgsave import download_and_name_img
 
@@ -32,10 +30,10 @@ class IArticleScraper(ABC):
         return BeautifulSoup(text, "lxml").text
 
 
-    def process_image(self, site_link, url):
+    def process_image(self, site_link, url, width: int = None, height: int = None):
         if not is_absolute(url):
             url = site_link + url
-        return download_and_name_img(url)
+        return download_and_name_img(url, width=width, height=height)
         
     def get_site_link(self, url):
         r = parse.urlparse(url)
